98f6c3bcaf84bc7d6b993798d0bf339d3d176a13
wzhao <cts-wzhao@live.com>
2015-01-16 17:04:19 +0800

    added the DB backup

--- LazyModule/LazyModule.cs
+++ LazyModule/LazyModule.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
+using System.Collections;
+using System.Diagnostics;
 
 namespace LazyModule
 {
     public class LazyModule : IHttpModule
     {
+
+        private static List<HttpContext> historyRequest = new List<HttpContext>();
+
         public void Dispose()
         {
             return;
@@ -17,8 +22,8 @@ namespace LazyModule
         public void Init(HttpApplication context)
         {
             context.PreRequestHandlerExecute += new EventHandler(OnPreRequestHandlerExecute);
-            //context.EndRequest += new EventHandler(OnEndRequest);
-            //context.BeginRequest += new EventHandler(OnBeginRequest);
+            context.EndRequest += new EventHandler(OnEndRequest);
+            context.BeginRequest += new EventHandler(PreSendRequestHeaders);
 
             context.PreSendRequestHeaders += new EventHandler(OnBeginRequest);
 
@@ -27,27 +32,68 @@ namespace LazyModule
             //System.Diagnostics.Trace.Listeners.Add(listener);
         }
 
-        private void OnPreRequestHandlerExecute(Object source, EventArgs e)
-        {
-            HttpApplication app = (HttpApplication)source;
-            HttpRequest request = app.Context.Request;
-
-            if (request.Url.ToString().ToUpper().Contains("ORDER/6"))
-            {
-                throw new System.SystemException("woops, I dare to reject you!");
-            }
-        }
-
         private void OnBeginRequest(object source, EventArgs e)
         {
             HttpApplication app = (HttpApplication)source;
             HttpRequest request = app.Context.Request;
 
+            Trace.TraceInformation("OnBeginRequest: " + request.Url.ToString());
             if (request.Url.ToString().ToUpper().Contains("ORDER/1"))
             {
                 System.Threading.Thread.Sleep(15000);
             }
         }
+
+        private void PreSendRequestHeaders(object source, EventArgs e)
+        {
+            HttpApplication app = (HttpApplication)source;
+            HttpRequest request = app.Context.Request;
+
+            Trace.TraceInformation("PreSendRequestHeaders: " + request.Url.ToString());
+            if (request.Url.ToString().ToUpper().Contains("ORDER/2"))
+            {
+                spinCPU();
+            }
+        }
+
+        private void OnPreRequestHandlerExecute(Object source, EventArgs e)
+        {
+            HttpApplication app = (HttpApplication)source;
+            HttpRequest request = app.Context.Request;
+
+            Trace.TraceInformation("OnPreRequestHandlerExecute: " + request.Url.ToString());
+
+            if (request.Url.ToString().ToUpper().Contains("ORDER/6") && !request.Url.ToString().ToUpper().Contains("ERROR"))
+            {
+                throw new System.SystemException("woops, I dare to reject you!");
+            }
+        }
+
+        private void OnEndRequest(Object source, EventArgs e)
+        {
+            HttpApplication app = (HttpApplication)source;
+            for (int i = 0; i < 200; i++)
+            {
+                historyRequest.Add(app.Context);
+            }
+            
+            Trace.TraceInformation("OnEndRequest: " + app.Context.Request.Url.ToString());
+        }
+
+        private void spinCPU()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Fib(40);
+            }
+        }
+
+        private int Fib(int n)
+        {
+            if (n < 3) return 1;
+            return Fib(n - 1) + Fib(n-2);
+        }
+
 
         //private void OnEndRequest(Object source, EventArgs e)
         //{
